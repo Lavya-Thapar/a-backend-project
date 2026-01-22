2c2f6bf523a491a9b318d62f992d6c8fa0d67b26
Lavya-Thapar <lavyathapar@gmail.com>
2026-01-22 17:04:02 +0530

    add subscription model with schema for user subscriptions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
-import { SchemaTypeOptions } from "mongoose"
+
 
 const generateAccessAndRefreshToken = async(userId)=>{
     try{
